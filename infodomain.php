7c15f5fbabff3b4ef8ed6604a47d6d5637b0855a
Ewout de Graaf <ewout@metaregistrar.com>
2013-10-18 11:27:36 +0200

    Created infodomain and signdomain code

---
+++ infodomain.php
@@ -0,0 +1,93 @@
+<?php
+// Base EPP objects
+include_once('Protocols/EPP/eppConnection.php');
+include_once('Protocols/EPP/eppRequests/eppIncludes.php');
+include_once('Protocols/EPP/eppResponses/eppIncludes.php');
+include_once('Protocols/EPP/eppData/eppIncludes.php');
+// Connection objects to registry servers - this contains your userids and passwords!
+include_once('Registries/Metaregistrar/metaregEppConnection.php');
+include_once('Registries/IIS/iisEppConnection.php');
+include_once('Registries/SIDN/sidnEppConnection.php');
+
+// Base EPP commands: hello, login and logout
+include_once('base.php');
+
+/*
+ * This script checks for the availability of domain names
+ *
+ * You can specify multiple domain names to be checked
+ */
+
+
+if ($argc <= 1)
+{
+    echo "Usage: infodomain.php <domainname>\n";
+	echo "Please enter a domain name retrieve\n\n";
+	die();
+}
+
+$domainname = $argv[1];
+
+echo "Retrieving info on ".$domainname."\n";
+try
+{
+    $conn = new sidnEppConnection(true);
+
+    // Connect to the EPP server
+    if ($conn->connect())
+    {
+        if (login($conn))
+        {
+            infodomain($conn, $domainname);
+            logout($conn);
+        }
+    }
+    else
+    {
+        echo "ERROR CONNECTING\n";
+    }
+}
+catch (eppException $e)
+{
+    echo "ERROR: ".$e->getMessage()."\n\n";
+}
+
+
+
+function infodomain($conn, $domainname)
+{
+	try
+	{
+        $epp = new eppDomain($domainname);
+		$info = new eppInfoDomainRequest($epp);
+		if ((($response = $conn->writeandread($info)) instanceof eppInfoDomainResponse) && ($response->Success()))
+		{
+            /* @var $response eppInfoDomainResponse */
+            $d = $response->getDomain();
+            echo "Info domain for ".$d->getDomainname().":\n";
+            echo "Created on ".$response->getDomainCreateDate()."\n";
+            echo "Last update on ".$response->getDomainUpdateDate()."\n";
+            echo "Registrant ".$d->getRegistrant()."\n";
+            echo "Contact info:\n";
+            foreach ($d->getContacts() as $contact)
+            {
+                echo "  ".$contact->getContactType().": ".$contact->getContactHandle()."\n";
+            }
+            echo "Nameserver info:\n";
+            foreach ($d->getHosts() as $nameserver)
+            {
+                echo "  ".$nameserver->getHostName()."\n";
+            }
+
+		}
+        else
+        {
+            echo "ERROR2\n";
+        }
+	}
+	catch (eppException $e)
+	{
+        echo 'ERROR1';
+		echo $e->getMessage()."\n";
+	}
+}
---
+++ signdomain.php
@@ -0,0 +1,51 @@
+<?php
+
+try
+{
+    $conn = new sidnEppConnection();
+    $this->eppConnect($conn);
+    $secadd = new eppSecdns();
+    $secadd->setKey('256','8','AwEAAbWM8nWQZbDZgJjyq+tLZwPLEXfZZjfvlRcmoAVZHgZJCPn/Ytu/iOsgci+yWgDT28ENzREAoAbKMflFFdhc5DNV27TZxhv8nMo9n2f+cyyRKbQ6oIAvMl7siT6WxrLxEBIMyoyFgDMbqGScn9k19Ppa8fwnpJgv0VUemfxGqHH9');
+    $domain = new eppDnssecUpdateRequest('dnssectransfer.nl',$secadd);
+    if ((($response = $conn->writeandread($domain)) instanceof eppUpdateResponse) && ($response->Success()))
+    {
+        echo "OKAY\n";
+    }
+    $this->eppDisconnect($conn);
+    return true;
+}
+catch (eppException $e)
+{
+    $this->eppDisconnect($conn);
+}
+
+
+function eppConnect()
+{
+    if ($conn->connect())
+    {
+        #
+        # Send greeting to EPP server
+        #
+        $login = new eppLoginRequest();
+        if ((($response = $conn->writeandread($login)) instanceof eppLoginResponse) && ($response->Success()))
+        {
+        }
+        else
+        {
+            throw new eppException('Unable to login to EPP');
+        }
+    }
+    else
+    {
+        throw new eppException('Unable to connect to EPP');
+    }
+}
+
+
+function eppDisconnect($conn)
+{
+    $logout = new eppLogoutRequest();
+    $conn->writeandread($logout);
+    $conn->disconnect();
+}
