1958e28452a5353f819cf6060dc34c6a9cdd36c1
Ewout de Graaf <ewout@metaregistrar.com>
2014-02-19 16:13:26 +0100

    Fixed euridEppExtensions

--- Registries/EURID/euridEppConnection.php
+++ Registries/EURID/euridEppConnection.php
@@ -30,12 +30,12 @@ class euridEppConnection extends eppConnection
         parent::setVersion('1.0');
         //parent::enableDnssec();
         parent::setServices(array('urn:ietf:params:xml:ns:domain-1.0'=>'domain','urn:ietf:params:xml:ns:contact-1.0'=>'contact'));
-        //parent::addExtension('http://www.eurid.eu/xml/epp/nsgroup-1.1','nsgroup');
+        //parent::addExtension('nsgroup','http://www.eurid.eu/xml/epp/nsgroup-1.1');
         parent::addService('http://www.eurid.eu/xml/epp/registrar-1.0','registrar');
-        
-        parent::addExtension('http://www.eurid.eu/xml/epp/contact-ext-1.0','contact-ext');
-        parent::addExtension('http://www.eurid.eu/xml/epp/domain-ext-1.0','domain-ext');
-        parent::addExtension('http://www.eurid.eu/xml/epp/authInfo-1.0','authInfo');
+
+        parent::addExtension('contact-ext','http://www.eurid.eu/xml/epp/contact-ext-1.0');
+        parent::addExtension('domain-ext','http://www.eurid.eu/xml/epp/domain-ext-1.0');
+        parent::addExtension('authInfo','http://www.eurid.eu/xml/epp/authInfo-1.0');
         #parent::addCommandResponse('euridEppCreateNsgroupRequest', 'euridEppCreateNsgroupResponse');
         #parent::addCommandResponse('euridEppCreateRequest', 'euridEppCreateResponse');
         #parent::addCommandResponse('euridEppAuthcodeRequest', 'eppResponse');
