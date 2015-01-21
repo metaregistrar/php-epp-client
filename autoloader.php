e43bf0ebb49205a9b3d45c6ac8b239f8663f5efc
Ewout de Graaf <ewout@metaregistrar.com>
2015-01-21 17:19:54 +0100

    Support for windows and linux file paths in autoloader

--- autoloader.php
+++ autoloader.php
@@ -4,8 +4,15 @@ date_default_timezone_set('UTC');
 require_once('base.php');
 
 function autoloadData($className) {
-    $fileName = str_replace('Metaregistrar\\EPP\\','',$className);
-    $fileName = __DIR__.'\Protocols\\EPP\\eppData\\'.$fileName.'.php';
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        $fileName = str_replace('Metaregistrar\\EPP\\','',$className);
+        $fileName = __DIR__.'\\Protocols\\EPP\\eppData\\'.$fileName.'.php';
+    }
+    else {
+        $fileName = str_replace('Metaregistrar/EPP/','',$className);
+        $fileName = __DIR__.'/Protocols/EPP/eppData/'.$fileName.'.php';
+    }
+
     //echo "Test autoload data $fileName\n";
     if (is_readable($fileName))
     {
@@ -16,8 +23,14 @@ function autoloadData($className) {
 }
 
 function autoloadRegistry($className) {
-    $fileName = str_replace('Metaregistrar\\EPP\\','',$className);
-    $fileName = __DIR__.'\\Registries\\'.$fileName.'\\eppConnection.php';
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        $fileName = str_replace('Metaregistrar\\EPP\\','',$className);
+        $fileName = __DIR__.'\\Registries\\'.$fileName.'\\eppConnection.php';
+    }
+    else {
+        $fileName = str_replace('Metaregistrar/EPP/','',$className);
+        $fileName = __DIR__.'/Registries/'.$fileName.'/eppConnection.php';
+    }
     if (is_readable($fileName))
     {
         //echo "Autoload registry $fileName\n";
@@ -26,25 +39,47 @@ function autoloadRegistry($className) {
 }
 
 function autoloadProtocol($className) {
-    $fileName = str_replace('Metaregistrar\\','',$className);
-    $fileName = __DIR__.'\\Protocols\\'.$fileName.'.php';
-    // Support for EPP Request file structure
-    if (strpos($className,'Request'))
-    {
-        $fileName = str_replace('Protocols\\EPP\\','Protocols\\EPP\\eppRequests\\',$fileName);
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        $fileName = str_replace('Metaregistrar\\','',$className);
+        $fileName = __DIR__.'\\Protocols\\'.$fileName.'.php';
+        // Support for EPP Request file structure
+        if (strpos($className,'Request'))
+        {
+            $fileName = str_replace('Protocols\\EPP\\','Protocols\\EPP\\eppRequests\\',$fileName);
+        }
+        // Support for EPP Response file structure
+        if (strpos($className,'Response'))
+        {
+            $fileName = str_replace('Protocols\\EPP\\','Protocols\\EPP\\eppResponses\\',$fileName);
+        }
+        // Support for EPP launch-1.0 file structure
+        if (strpos($className,'eppLaunch'))
+        {
+            $fileName = str_replace('Protocols\\EPP\\eppRequests\\','Protocols\\EPP\\eppRequests\\launch-1.0\\',$fileName);
+            $fileName = str_replace('Protocols\\EPP\\eppResponses\\','Protocols\\EPP\\eppResponses\\launch-1.0\\',$fileName);
+        }
     }
-    // Support for EPP Response file structure
-    if (strpos($className,'Response'))
-    {
-        $fileName = str_replace('Protocols\\EPP\\','Protocols\\EPP\\eppResponses\\',$fileName);
+    else {
+        $fileName = str_replace('Metaregistrar/','',$className);
+        $fileName = __DIR__.'/Protocols/'.$fileName.'.php';
+        // Support for EPP Request file structure
+        if (strpos($className,'Request'))
+        {
+            $fileName = str_replace('Protocols/EPP/','Protocols/EPP/eppRequests/',$fileName);
+        }
+        // Support for EPP Response file structure
+        if (strpos($className,'Response'))
+        {
+            $fileName = str_replace('Protocols/EPP/','Protocols/EPP/eppResponses/',$fileName);
+        }
+        // Support for EPP launch-1.0 file structure
+        if (strpos($className,'eppLaunch'))
+        {
+            $fileName = str_replace('Protocols/EPP/eppRequests/','Protocols/EPP/eppRequests/launch-1.0/',$fileName);
+            $fileName = str_replace('Protocols/EPP/eppResponses/','Protocols/EPP/eppResponses/launch-1.0/',$fileName);
+        }
     }
-    // Support for EPP launch-1.0 file structure
-    if (strpos($className,'eppLaunch'))
-    {
-        $fileName = str_replace('Protocols\\EPP\\eppRequests\\','Protocols\\EPP\\eppRequests\\launch-1.0\\',$fileName);
-        $fileName = str_replace('Protocols\\EPP\\eppResponses\\','Protocols\\EPP\\eppResponses\\launch-1.0\\',$fileName);
 
-    }
     //echo "Test autoload protocol $fileName\n";
     if (is_readable($fileName))
     {
