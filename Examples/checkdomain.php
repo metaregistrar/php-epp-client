3e05032fdc107fea1640411f659230dbd2a5e699
Metaregistrar <ewout@metaregistrar.com>
2015-12-03 15:22:47 +0100

    restructured try-catch blocks in the examples

--- Examples/checkdomain.php
+++ Examples/checkdomain.php
@@ -6,7 +6,6 @@ require('../autoloader.php');
  * You can specify multiple domain names to be checked
  */
 
-
 if ($argc <= 1) {
     echo "Usage: checkdomain.php <domainnames>\n";
     echo "Please enter one or more domain names to check\n\n";
@@ -21,6 +20,7 @@ echo "Checking " . count($domains) . " domain names\n";
 try {
     $conn = new Metaregistrar\EPP\metaregEppConnection(true);
     // Set login details for the service in the form of
+    // interface=metaregEppConnection
     // hostname=ssl://epp.test2.metaregistrar.com
     // port=7443
     // userid=xxxxxxxx
@@ -38,27 +38,18 @@ try {
 
 /**
  * @param $conn Metaregistrar\EPP\eppConnection
- * @param $domains array
+ * @param $domains array of domain names
  */
 function checkdomains($conn, $domains) {
-    try {
-        // Create request to be sent to EPP service
-        $check = new Metaregistrar\EPP\eppCheckRequest($domains);
-        // Write request to EPP service, read and check the results
-        if ((($response = $conn->writeandread($check)) instanceof Metaregistrar\EPP\eppCheckResponse) && ($response->Success())) {
-            /* @var $response Metaregistrar\EPP\eppCheckResponse */
-            // Walk through the results
-            $checks = $response->getCheckedDomains();
-            foreach ($checks as $check) {
-                echo $check['domainname'] . " is " . ($check['available'] ? 'free' : 'taken') . " (" . $check['reason'] . ")\n";
-            }
-        } else {
-            // No valid response received from EPP service
-            echo "ERROR2\n";
+    // Create request to be sent to EPP service
+    $check = new Metaregistrar\EPP\eppCheckRequest($domains);
+    // Write request to EPP service, read and check the results
+    if ((($response = $conn->writeandread($check)) instanceof Metaregistrar\EPP\eppCheckResponse) && ($response->Success())) {
+        /* @var $response Metaregistrar\EPP\eppCheckResponse */
+        // Walk through the results
+        $checks = $response->getCheckedDomains();
+        foreach ($checks as $check) {
+            echo $check['domainname'] . " is " . ($check['available'] ? 'free' : 'taken') . " (" . $check['reason'] . ")\n";
         }
-    } catch (Metaregistrar\EPP\eppException $e) {
-        // Well-formatted error received from EPP service
-        echo 'ERROR1';
-        echo $e->getMessage() . "\n";
     }
 }
