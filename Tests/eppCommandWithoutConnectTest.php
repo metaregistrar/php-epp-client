1d3b02d8105327d1b039d582c559c17533c03507
Metaregistrar <ewout@metaregistrar.com>
2016-01-18 17:23:58 +0100

    Test for command without logging in

---
+++ Tests/eppCommandWithoutConnectTest.php
@@ -0,0 +1,22 @@
+<?php
+include_once(dirname(__FILE__).'/eppTestCase.php');
+
+class eppCommandWithoutConnectTest extends eppTestCase {
+
+    public function testCommandWithoutConnection()
+    {
+        $conn = new Metaregistrar\EPP\metaregEppConnection(false);
+        $conn->setHostname('ssl://epp.test2.metaregistrar.com');
+        $conn->setPort(7443);
+        if ($conn->connect()) {
+            $domain = new Metaregistrar\EPP\eppDomain('fasfasfasfashfgaf.guru');
+            $info = new Metaregistrar\EPP\eppInfoDomainRequest($domain);
+            $response = $conn->writeandread($info);
+            $this->assertInstanceOf('Metaregistrar\EPP\eppInfoResponse',$response);
+            if ($response instanceof Metaregistrar\EPP\eppInfoResponse) {
+                $this->setExpectedException('Metaregistrar\epp\eppException','Error 2202: Invalid authorization information');
+                $this->assertFalse($response->Success());
+            }
+        }
+     }
+}
