6526c796aaef4a9bda1b123ff975ab808ee2aeb3
Alexander Sagen <alexander@konsept-it.no>
2017-01-16 14:40:47 +0100

    Add Norid transfer request with operation execute
    Also clean up Norid withdraw request by adding more to the Norid domain
    request trait.

--- Protocols/EPP/eppExtensions/norid/eppRequests/noridEppWithdrawDomainRequest.php
+++ Protocols/EPP/eppExtensions/norid/eppRequests/noridEppWithdrawDomainRequest.php
@@ -12,23 +12,19 @@ class noridEppWithdrawContactRequest extends eppRequest {
     function __construct(noridEppDomain $domain, $namespacesinroot = true) {
         $this->setNamespacesinroot($namespacesinroot);
         parent::__construct();
-        $this->domainobject = $this->createElement('domain:withdraw');
-        if (!$this->rootNamespaces()) {
-            $this->domainobject->setAttribute('xmlns:no-ext-domain', 'http://www.norid.no/xsd/no-ext-domain-1.1');
-        }
-        $commandobject = $this->createElement('command');
-        if (!$this->rootNamespaces()) {
-            $commandobject->setAttribute('xmlns', 'http://www.norid.no/xsd/no-ext-epp-1.0');
-        }
-        $this->getEpp()->appendChild($this->createElement('extension')->appendChild($commandobject->appendChild($this->createElement('withdraw')->appendChild($this->domainobject))));
         $this->setDomain($domain);
         $this->addSessionId();
     }
 
     public function setDomain(noridEppDomain $domain) {
-        if (!is_null($domain)) {
-            $this->domainobject->appendChild($this->createElement('domain:name', $domain->getDomainname()));
+        $withdraw = $this->createElement('withdraw');
+        $this->domainobject = $this->createElement('domain:withdraw');
+        if (!$this->rootNamespaces()) {
+            $this->domainobject->setAttribute('xmlns:no-ext-domain', 'http://www.norid.no/xsd/no-ext-domain-1.1');
         }
+        $this->domainobject->appendChild($this->createElement('domain:name', $domain->getDomainname()));
+        $withdraw->appendChild($this->domainobject);
+        $this->getExtCommand()->appendChild($withdraw);
     }
 
 }
