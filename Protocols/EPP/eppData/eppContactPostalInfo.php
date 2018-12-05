b36f2ea0d6e9ddcc2b15d08f9a3b176596791958
thomasmeike <thomas.meike@nic.at>
2018-12-05 11:21:00 +0100

    Update eppContactPostalInfo.php
    checking existence of an array value for streets in if condition via isset is the more reliable approach here

--- Protocols/EPP/eppData/eppContactPostalInfo.php
+++ Protocols/EPP/eppData/eppContactPostalInfo.php
@@ -75,7 +75,7 @@ class eppContactPostalInfo {
      * @return string
      */
     public function getStreet($line) {
-        if ($this->street[$line]) {
+        if (isset($this->street[$line])) {
             return $this->street[$line];
         }
         return null;
