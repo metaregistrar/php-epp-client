566fd6309390f8667e0fc7a89cf66fe4242c1e29
Ewout de Graaf <ewout@metaregistrar.com>
2019-02-13 16:29:42 +0100

    charge-1.0 extension added

--- Registries/cozaEppConnection/eppConnection.php
+++ Registries/cozaEppConnection/eppConnection.php
@@ -7,6 +7,7 @@ class cozaEppConnection extends eppConnection {
     public function __construct($logging = false, $settingsfile = null) {
         parent::__construct($logging, $settingsfile);
         parent::useExtension('cozacontact-1.0');
+        parent::useExtension('charge-1.0');
         parent::enableDnssec();
     }
 
