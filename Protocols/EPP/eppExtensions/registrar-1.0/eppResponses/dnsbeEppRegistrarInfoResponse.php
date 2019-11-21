c89b60a16471a0fae3fb8f06fbdcc812509ddae8
Marc <marc@bhosted.nl>
2019-11-21 15:14:39 +0100

    - New: dnsbe RegistrarInfo request for getting balance

--- Protocols/EPP/eppExtensions/registrar-1.0/eppResponses/dnsbeEppRegistrarInfoResponse.php
+++ Protocols/EPP/eppExtensions/registrar-1.0/eppResponses/dnsbeEppRegistrarInfoResponse.php
@@ -11,13 +11,34 @@ class dnsbeEppRegistrarInfoResponse extends eppResponse {
         parent::__construct();
     }
 
-    public function getBalance() {
+    public function getAmountAvailable() {
         $xpath = $this->xPath();
-        $result = $xpath->query('//dnssi:balance');
+        $result = $xpath->query('//dnsbe:amountAvailable');
         if ($result->length > 0) {
             return $result->item(0)->nodeValue;
         } else {
             return null;
         }
     }
+
+    public function getHitPoints() {
+        $xpath = $this->xPath();
+        $result = $xpath->query('//dnsbe:nbrHitPoints');
+        if ($result->length > 0) {
+            return $result->item(0)->nodeValue;
+        } else {
+            return null;
+        }
+    }
+
+    public function getMaxHitPoints() {
+        $xpath = $this->xPath();
+        $result = $xpath->query('//dnsbe:maxNbrHitPoints');
+        if ($result->length > 0) {
+            return $result->item(0)->nodeValue;
+        } else {
+            return null;
+        }
+    }
+
 }
--- Protocols/EPP/eppExtensions/registrar-1.0/includes.php
+++ Protocols/EPP/eppExtensions/registrar-1.0/includes.php
@@ -3,10 +3,14 @@ $this->addExtension('registrar', 'http://www.arnes.si/xml/epp/registrar-1.0');
 
 include_once(dirname(__FILE__) . '/eppRequests/siEppRegistrarInfoRequest.php');
 include_once(dirname(__FILE__) . '/eppResponses/siEppRegistrarInfoResponse.php');
+
 $this->addCommandResponse('Metaregistrar\EPP\siEppRegistrarInfoRequest', 'Metaregistrar\EPP\siEppRegistrarInfoResponse');
+
+$this->addExtension('registrar', 'http://www.dns.be/xml/epp/registrar-1.0');
 
 include_once(dirname(__FILE__) . '/eppRequests/dnsbeEppRegistrarInfoRequest.php');
 include_once(dirname(__FILE__) . '/eppResponses/dnsbeEppRegistrarInfoResponse.php');
+
 $this->addCommandResponse('Metaregistrar\EPP\dnsbeEppRegistrarInfoRequest', 'Metaregistrar\EPP\dnsbeEppRegistrarInfoResponse');
 
 
