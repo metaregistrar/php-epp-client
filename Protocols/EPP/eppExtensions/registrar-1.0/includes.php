e08ba528f21b50cd7e2187ecf928dc6e563be38e
Marc <marc@bhosted.nl>
2019-11-21 14:50:45 +0100

    - New: dnsbe RegistrarInfo request for getting balance

--- Protocols/EPP/eppExtensions/registrar-1.0/includes.php
+++ Protocols/EPP/eppExtensions/registrar-1.0/includes.php
@@ -3,7 +3,11 @@ $this->addExtension('registrar', 'http://www.arnes.si/xml/epp/registrar-1.0');
 
 include_once(dirname(__FILE__) . '/eppRequests/siEppRegistrarInfoRequest.php');
 include_once(dirname(__FILE__) . '/eppResponses/siEppRegistrarInfoResponse.php');
-
 $this->addCommandResponse('Metaregistrar\EPP\siEppRegistrarInfoRequest', 'Metaregistrar\EPP\siEppRegistrarInfoResponse');
+
+include_once(dirname(__FILE__) . '/eppRequests/dnsbeEppRegistrarInfoRequest.php');
+include_once(dirname(__FILE__) . '/eppResponses/dnsbeEppRegistrarInfoResponse.php');
+$this->addCommandResponse('Metaregistrar\EPP\dnsbeEppRegistrarInfoRequest', 'Metaregistrar\EPP\dnsbeEppRegistrarInfoResponse');
+
 
 
