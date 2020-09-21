3b4986c19462334c3052de810d9f262473f63603
Ewout de Graaf <ewout@metaregistrar.com>
2020-09-21 17:28:39 +0200

    Retrieve schedule delete date from SIDN domain info command

--- Protocols/EPP/eppExtensions/sidn-ext-epp-1.0/eppResponses/sidnEppInfoDomainResponse.php
+++ Protocols/EPP/eppExtensions/sidn-ext-epp-1.0/eppResponses/sidnEppInfoDomainResponse.php
@@ -9,7 +9,7 @@ class sidnEppInfoDomainResponse extends eppInfoDomainResponse {
 
     /**
      *
-     * @return string Period
+     * @return string Period|null
      */
     public function getDomainPeriod() {
         $xpath = $this->xPath();
@@ -23,7 +23,7 @@ class sidnEppInfoDomainResponse extends eppInfoDomainResponse {
 
     /**
      *
-     * @return string Optout
+     * @return string Optout|null
      */
     public function getDomainOptout() {
         $xpath = $this->xPath();
@@ -37,11 +37,25 @@ class sidnEppInfoDomainResponse extends eppInfoDomainResponse {
 
     /**
      *
-     * @return string Limited
+     * @return string Limited|null
      */
     public function getDomainLimited() {
         $xpath = $this->xPath();
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/sidn-ext-epp:ext/sidn-ext-epp:infData/sidn-ext-epp:domain/sidn-ext-epp:limited');
+        if ($result->length > 0) {
+            return $result->item(0)->nodeValue;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @return string DeleteDate|null
+     */
+    public function getDomainDeleteDate() {
+        $xpath = $this->xPath();
+        $result = $xpath->query('/epp:epp/epp:response/epp:extension/sidn-ext-epp:ext/sidn-ext-epp:infData/sidn-ext-epp:domain/sidn-ext-epp:scheduledDeleteDate');
         if ($result->length > 0) {
             return $result->item(0)->nodeValue;
         } else {
