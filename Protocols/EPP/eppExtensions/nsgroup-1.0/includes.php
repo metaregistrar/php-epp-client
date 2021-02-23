6163ebf5d42c177c2c35967d98fd3d1c85eaefd2
Bruno Mairlot <bruno@maehdros.com>
2021-02-23 18:54:22 +0100

    Added the DeleteNsgroup Request and Response

--- Protocols/EPP/eppExtensions/nsgroup-1.0/includes.php
+++ Protocols/EPP/eppExtensions/nsgroup-1.0/includes.php
@@ -10,3 +10,8 @@ include_once(dirname(__FILE__) . '/eppRequests/dnsbeEppCreateNsgroupRequest.php'
 include_once(dirname(__FILE__) . '/eppResponses/dnsbeEppCreateNsgroupResponse.php');
 
 $this->addCommandResponse('Metaregistrar\EPP\dnsbeEppCreateNsgroupRequest', 'Metaregistrar\EPP\dnsbeEppCreateNsgroupResponse');
+
+include_once(dirname(__FILE__) . '/eppRequests/dnsbeEppDeleteNsgroupRequest.php');
+include_once(dirname(__FILE__) . '/eppResponses/dnsbeEppDeleteNsgroupResponse.php');
+
+$this->addCommandResponse('Metaregistrar\EPP\dnsbeEppDeleteNsgroupRequest', 'Metaregistrar\EPP\dnsbeEppDeleteNsgroupResponse');
