7c5869202dada009fbe4cf094a6e186fa217bfba
Uros Gruber <uros.gruber@dlabs.si>
2022-03-26 21:37:38 +0100

    fix broken Arnes implementation

--- Protocols/EPP/eppExtensions/registrar-1.0/includes.php
+++ Protocols/EPP/eppExtensions/registrar-1.0/includes.php
@@ -1,17 +1,22 @@
 <?php
-$this->addExtension('registrar', 'http://www.arnes.si/xml/epp/registrar-1.0');
+$bt = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 2);
+$reflect = new ReflectionClass($bt[1]['object']);
 
-include_once(dirname(__FILE__) . '/eppRequests/siEppRegistrarInfoRequest.php');
-include_once(dirname(__FILE__) . '/eppResponses/siEppRegistrarInfoResponse.php');
+if ($reflect->getShortName() === 'siEppConnection') {
+    $this->addExtension('registrar', 'http://www.arnes.si/xml/epp/registrar-1.0');
 
-$this->addCommandResponse('Metaregistrar\EPP\siEppRegistrarInfoRequest', 'Metaregistrar\EPP\siEppRegistrarInfoResponse');
+    include_once(dirname(__FILE__) . '/eppRequests/siEppRegistrarInfoRequest.php');
+    include_once(dirname(__FILE__) . '/eppResponses/siEppRegistrarInfoResponse.php');
 
-$this->addExtension('registrar', 'http://www.dns.be/xml/epp/registrar-1.0');
+    $this->addCommandResponse('Metaregistrar\EPP\siEppRegistrarInfoRequest', 'Metaregistrar\EPP\siEppRegistrarInfoResponse');
+} else {
+    $this->addExtension('registrar', 'http://www.dns.be/xml/epp/registrar-1.0');
 
-include_once(dirname(__FILE__) . '/eppRequests/dnsbeEppRegistrarInfoRequest.php');
-include_once(dirname(__FILE__) . '/eppResponses/dnsbeEppRegistrarInfoResponse.php');
+    include_once(dirname(__FILE__) . '/eppRequests/dnsbeEppRegistrarInfoRequest.php');
+    include_once(dirname(__FILE__) . '/eppResponses/dnsbeEppRegistrarInfoResponse.php');
 
-$this->addCommandResponse('Metaregistrar\EPP\dnsbeEppRegistrarInfoRequest', 'Metaregistrar\EPP\dnsbeEppRegistrarInfoResponse');
+    $this->addCommandResponse('Metaregistrar\EPP\dnsbeEppRegistrarInfoRequest', 'Metaregistrar\EPP\dnsbeEppRegistrarInfoResponse');
 
+}
 
 
