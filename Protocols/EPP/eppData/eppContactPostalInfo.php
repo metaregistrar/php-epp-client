599bcbaeb8af0a8aecb25d1e5e1b9be545e51689
Worma <beck.worma@gmail.com>
2022-12-07 08:58:02 +0100

    Call setName() only if the provided name is not null
    This fixes a warning in PHP 8.1: htmlspecialchars(): Passing null to parameter #1 ($string) of type string is deprecated

--- Protocols/EPP/eppData/eppContactPostalInfo.php
+++ Protocols/EPP/eppData/eppContactPostalInfo.php
@@ -33,7 +33,9 @@ class eppContactPostalInfo {
      * @param string $type POSTAL_TYPE_LOC or POSTAL_TYPE_INT
      */
     public function __construct($name = null, $city = null, $countrycode = null, $organisationName = null, $street = null, $province = null, $zipcode = null, $type = eppContact::TYPE_AUTO) {
-        $this->setName($name);
+        if (null !== $name) {
+            $this->setName($name);
+        }
         #
         # Street can be an array of max 3 streets, or a string with an address
         #
