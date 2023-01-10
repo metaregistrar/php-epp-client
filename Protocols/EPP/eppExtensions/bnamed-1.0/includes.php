fb741787625fd7667e9b0dd257701f81c46d21f1
bHosted <50988060+mvdgeijn@users.noreply.github.com>
2023-01-10 16:35:31 +0100

    Update includes.php

--- Protocols/EPP/eppExtensions/bnamed-1.0/includes.php
+++ Protocols/EPP/eppExtensions/bnamed-1.0/includes.php
@@ -1,4 +1,5 @@
 <?php
 include_once(dirname(__FILE__) . '/eppRequests/bNamedEppCreateDomainRequest.php');
 $this->addCommandResponse('Metaregistrar\EPP\bNamedEppCreateDomainRequest', 'Metaregistrar\EPP\eppCreateDomainResponse');
+$this->addCommandResponse('Metaregistrar\EPP\bNamedEppUndeleteDomainRequest', 'Metaregistrar\EPP\eppUndeleteDomainResponse');
 
