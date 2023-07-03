dd13f4e66300563de5d3e54126d6f5ceeac454eb
Eugene Surov <eugene.surov@nic.at>
2023-07-03 14:04:46 +0200

    Added domain withdraw request and response

---
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppResponses/atEppWithdrawResponse.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martinha
+ * Date: 10/04/2019
+ * Time: 09:32
+ */
+
+namespace Metaregistrar\EPP;
+
+/**
+ * Class atEppWithdrawResponse represent the at-registry specific withdraw response.
+ *
+ * @package Metaregistrar\EPP
+ */
+class atEppWithdrawResponse extends eppResponse
+{
+    use atEppResponseTrait;
+}
