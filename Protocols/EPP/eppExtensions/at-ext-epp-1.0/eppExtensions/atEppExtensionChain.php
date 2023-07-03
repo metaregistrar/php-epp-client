9b229f3dabde3de1efc0d2b8fed1cf513385f382
Eugene Surov <eugene.surov@nic.at>
2023-07-03 15:07:28 +0200

    Typo fixed.

---
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppExtensions/atEppExtensionChain.php
@@ -0,0 +1,24 @@
+<?php
+namespace Metaregistrar\EPP;
+
+/**
+ * Created by PhpStorm.
+ * User: thomasm
+ * Date: 11.09.2015
+ * Time: 09:27
+ */
+abstract class atEppExtensionChain
+{
+    protected $additionalEppExtension=null;
+
+    function __construct(atEppExtensionChain $additionalEppExtension=null) {
+
+        $this->additionalEppExtension = $additionalEppExtension;
+    }
+
+    public function setEppRequestExtension(eppRequest $request,\DOMElement $extension)
+    {
+
+
+    }
+}
--- Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppDeleteRequest.php
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppDeleteRequest.php
@@ -6,10 +6,9 @@ class atEppDeleteRequest extends eppDeleteRequest
 {
     use atEppCommandTrait;
 
-    protected $atEppExtensionChain = null;
-
-    function __construct($deleteinfo,atEppExtensionChain $atEppExtensionChain=null) {
+    function __construct($deleteinfo, atEppExtensionChain $atEppExtensionChain = null) {
         $this->atEppExtensionChain = $atEppExtensionChain;
+
         parent::__construct($deleteinfo);
         $this->setAtExtensions();
         $this->addSessionId();
