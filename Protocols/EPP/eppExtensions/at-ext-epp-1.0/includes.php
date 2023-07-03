0ff5371ec6df98a5a99912b9725b5d8d4cbf906e
Eugene Surov <eugene.surov@nic.at>
2023-07-03 15:27:21 +0200

    Added missing requests and response classes to the includes.php

--- Protocols/EPP/eppExtensions/at-ext-epp-1.0/includes.php
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/includes.php
@@ -4,6 +4,17 @@ include_once(dirname(__FILE__) . '/eppRequests/atEppCreateContactRequest.php');
 include_once(dirname(__FILE__) . '/eppRequests/atEppUpdateContactRequest.php');
 include_once(dirname(__FILE__) . '/eppRequests/atEppCreateDomainRequest.php');
 include_once(dirname(__FILE__) . '/eppRequests/atEppUpdateDomainRequest.php');
+include_once(dirname(__FILE__) . '/eppRequests/atEppDeleteRequest.php');
+include_once(dirname(__FILE__) . '/eppRequests/atEppTransferRequest.php');
+include_once(dirname(__FILE__) . '/eppRequests/atEppUndeleteRequest.php');
+include_once(dirname(__FILE__) . '/eppRequests/atEppWithdrawRequest.php');
+
 include_once(dirname(__FILE__) . '/eppResponses/atEppCreateResponse.php');
 include_once(dirname(__FILE__) . '/eppResponses/atEppUpdateContactResponse.php');
 include_once(dirname(__FILE__) . '/eppResponses/atEppUpdateDomainResponse.php');
+include_once(dirname(__FILE__) . '/eppResponses/atEppDeleteResponse.php');
+include_once(dirname(__FILE__) . '/eppResponses/atEppInfoContactResponse.php');
+include_once(dirname(__FILE__) . '/eppResponses/atEppInfoDomainResponse.php');
+include_once(dirname(__FILE__) . '/eppResponses/atEppTransferResponse.php');
+include_once(dirname(__FILE__) . '/eppResponses/atEppUndeleteResponse.php');
+include_once(dirname(__FILE__) . '/eppResponses/atEppWithdrawResponse.php');
