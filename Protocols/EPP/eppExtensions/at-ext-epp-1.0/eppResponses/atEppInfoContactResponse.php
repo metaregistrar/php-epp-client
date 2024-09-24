a4541131621fee854834ea5fd18fef65001ac759
Michael Braunoeder <mib@nic.at>
2024-09-24 15:52:02 +0200

    add verification info to info contact

--- Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppResponses/atEppInfoContactResponse.php
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppResponses/atEppInfoContactResponse.php
@@ -46,7 +46,7 @@ class atEppInfoContactResponse extends eppInfoContactResponse
     {
 
         $xpath = $this->xPath();
-       $xpath->registerNamespace ( "at-ext-contact" , atEppConstants::namespaceAtExtContact );
+        $xpath->registerNamespace ( "at-ext-contact" , atEppConstants::namespaceAtExtContact );
 
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/at-ext-contact:infData/at-ext-contact:type');
         if (!is_null($result) && $result->length > 0) {
@@ -56,6 +56,56 @@ class atEppInfoContactResponse extends eppInfoContactResponse
         }
     }
 
+    public function getValidationReport()
+    {
+        $xpath = $this->xPath();
+        $xpath->registerNamespace ( "at-ext-verification" , atEppConstants::namespaceAtExtVerification );
 
+        $result = $xpath->query('/epp:epp/epp:response/epp:extension/at-ext-verification:infData/at-ext-verification:report');
+        if (!is_null($result) && $result->length > 0) {
+            $verificationReport=new atEppVerificationReport();
+            $verificationReport->setReceivedDate($result->item(0)->getAttribute('receivedDate'));
+            $verificationReport->setclID($result->item(0)->getAttribute('clID'));
+            $verificationReport->setResult($result->item(0)->getElementsByTagName('result')->item(0)->nodeValue);
+            $verificationReport->setVerificationDate($result->item(0)->getElementsByTagName('verificationDate')->item(0)->nodeValue);
+            $verificationReport->setMethod($result->item(0)->getElementsByTagName('method')->item(0)->nodeValue);
+            $verificationReport->setReference($result->item(0)->getElementsByTagName('reference')->item(0)->nodeValue);
+            $verificationReport->setAgent($result->item(0)->getElementsByTagName('agent')->item(0)->nodeValue);
+
+            return $verificationReport;
+        }
+
+        return null;
+
+    }
+
+    public function getValidationStatus()
+    {
+        $xpath = $this->xPath();
+        $xpath->registerNamespace ( "at-ext-verification" , atEppConstants::namespaceAtExtVerification );
+
+        $result = $xpath->query('/epp:epp/epp:response/epp:extension/at-ext-verification:infData/at-ext-verification:status/@s');
+        if (!is_null($result) && $result->length > 0) {
+            return $result->item(0)->nodeValue;
+        }
+
+        return null;
+    }
+    
+    public function getValidationActionDate()
+    {
+        $xpath = $this->xPath();
+        $xpath->registerNamespace ( "at-ext-verification" , atEppConstants::namespaceAtExtVerification );
+
+        $result = $xpath->query('/epp:epp/epp:response/epp:extension/at-ext-verification:infData/at-ext-verification:actionDate');
+        if (!is_null($result) && $result->length > 0) {
+            return $result->item(0)->nodeValue;
+        }
+
+        return null;
+    }
+    
+    
+    
 
 }
