5122566bbbebe351c839f01103c5492d052fd183
awyld <awyld@easydns.com>
2025-02-24 15:44:30 -0500

    - Found a much better way to provide access to keysys result data by moving methods to eppInfoDomainResponse - Updated required PHP version to 7.1 which is min version with support for null typing

--- Protocols/EPP/eppResponses/eppInfoDomainResponse.php
+++ Protocols/EPP/eppResponses/eppInfoDomainResponse.php
@@ -276,4 +276,36 @@ class eppInfoDomainResponse extends eppInfoResponse {
         return null;
     }
 
+    /**
+     * Get the renewal mode from the returned keysys data. Should actually be put under extensions/keysys but this is always the object returned!
+     * @return string|null
+     */
+    public function getRenewalMode() : ?string
+    {
+        // Check if keysys is enabled on this interface
+        if ($this->findNamespace('keysys')) {
+            $xpath = $this->xPath();
+            $results = $xpath->query('/epp:epp/epp:response/epp:extension/keysys:resData/keysys:infData/keysys:renewalmode');
+            foreach ($results as $result) {
+                return $result->nodeValue;
+            }
+        }
+    }
+
+    /**
+     * Get the transfer mode from the returned keysys data. Should actually be put under extensions/keysys but this is always the object returned!
+     * @return string|null
+     */
+    public function getTransferMode() : ?string
+    {
+        // Check if keysys is enabled on this interface
+        if ($this->findNamespace('keysys')) {
+            $xpath = $this->xPath();
+            $results = $xpath->query('/epp:epp/epp:response/epp:extension/keysys:resData/keysys:infData/keysys:transfermode');
+            foreach ($results as $result) {
+                return $result->nodeValue;
+            }
+        }
+    }
+
 }
