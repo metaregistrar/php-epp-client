e2821baa64233e7c29f1d608c84b1e7b39d740d9
awyld <awyld@easydns.com>
2025-02-24 15:23:01 -0500

    Forced to extend RGP info domain response to allow access to renewal/transfer mode part of keysys extension because that is what is returned when getting domain info with RGP extension enabled.

--- Protocols/EPP/eppExtensions/rgp-1.0/eppResponses/eppRgpInfoDomainResponse.php
+++ Protocols/EPP/eppExtensions/rgp-1.0/eppResponses/eppRgpInfoDomainResponse.php
@@ -18,4 +18,32 @@ class eppRgpInfoDomainResponse extends eppInfoDomainResponse
     }
     return $statuses;
   }
+
+    /**
+     * Get the renewal mode from the returned keysys data. Should actually be put under extensions/keysys but this is always the object returned!
+     * @return string|null
+     */
+    public function getRenewalMode() : ?string
+    {
+        $xpath = $this->xPath();
+        $results = $xpath->query('/epp:epp/epp:response/epp:extension/keysys:resData/keysys:infData/keysys:renewalmode');
+        foreach($results as $result)
+        {
+            return $result->nodeValue;
+        }
+    }
+
+    /**
+     * Get the transfer mode from the returned keysys data. Should actually be put under extensions/keysys but this is always the object returned!
+     * @return string|null
+     */
+    public function getTransferMode() : ?string
+    {
+        $xpath = $this->xPath();
+        $results = $xpath->query('/epp:epp/epp:response/epp:extension/keysys:resData/keysys:infData/keysys:transfermode');
+        foreach($results as $result)
+        {
+            return $result->nodeValue;
+        }
+    }
 }
