e36aafac198d7ee7ef1421399e5517e1ea059057
Brianvanwessel <31928901+Brianvanwessel@users.noreply.github.com>
2025-04-28 11:18:57 +0200

    Improve euridEppUpdateDomainRequest class

--- Protocols/EPP/eppExtensions/domain-ext-2.5/eppRequests/euridEppUpdateDomainRequest.php
+++ Protocols/EPP/eppExtensions/domain-ext-2.5/eppRequests/euridEppUpdateDomainRequest.php
@@ -4,6 +4,47 @@ namespace Metaregistrar\EPP;
 
 use DOMElement;
 
+/*
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<epp
+    xmlns="urn:ietf:params:xml:ns:epp-1.0"
+    xmlns:eurid="http://www.eurid.eu/xml/epp/eurid-1.0">
+    <command>
+        <update>
+            <domain:update
+                xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
+                <domain:name>somedomain</domain:name>
+                <domain:add>
+                    <domain:ns>
+                        <domain:hostAttr>
+                            <domain:hostName>ns1.abc.eu</domain:hostName>
+                        </domain:hostAttr>
+                        <domain:hostAttr>
+                            <domain:hostName>ns2.somedomain.eu</domain:hostName>
+                            <domain:hostAddr ip="v4">178.32.172.146</domain:hostAddr>
+                        </domain:hostAttr>
+                        <domain:hostAttr>
+                            <domain:hostName>ns3.somedomain.eu</domain:hostName>
+                            <domain:hostAddr ip="v6">2001:67c:9c:1::184</domain:hostAddr>
+                        </domain:hostAttr>
+                    </domain:ns>
+                    <domain:contact type="tech">c320</domain:contact>
+                    <domain:contact type="tech">c321</domain:contact>
+                </domain:add>
+            </domain:update>
+        </update>
+        <extension>
+            <domain-ext:update
+                xmlns:domain-ext="http://www.eurid.eu/xml/epp/domain-ext-2.5">
+                <domain-ext:add>
+                    <domain-ext:nsgroup>nsgroup-1573042729356</domain-ext:nsgroup>
+                </domain-ext:add>
+            </domain-ext:update>
+        </extension>
+        <clTRID>domain-update02</clTRID>
+    </command>
+</epp>
+*/
 class euridEppUpdateDomainRequest extends eppUpdateDomainRequest
 {
     public function __construct(
@@ -13,36 +54,36 @@ class euridEppUpdateDomainRequest extends eppUpdateDomainRequest
         ?eppDomain $updateinfo = null,
         bool $forcehostattr = true,
         bool $namespacesinroot = true,
+        bool $usecdata = true,
         string|array|null $addnsgroup = null,
         string|array|null $removensgroup = null
     ) {
-        parent::__construct($objectname, $addinfo, $removeinfo, $updateinfo, $forcehostattr, $namespacesinroot);
-        $this->updatensgroup($addnsgroup, $removensgroup);
+        parent::__construct($objectname, $addinfo, $removeinfo, $updateinfo, $forcehostattr, $namespacesinroot, $usecdata);
+        $this->updateNameserverGroups($addnsgroup, $removensgroup);
         parent::addSessionId();
     }
 
-    public function updatensgroup(string|array|null $addnsgroup = null, string|array|null $removensgroup = null): void
+    public function updateNameserverGroups(string|array|null $addnsgroup = null, string|array|null $removensgroup = null): void
     {
         if ($addnsgroup === null && $removensgroup === null) {
             return;
         }
 
-        $create = $this->createElement('domain-ext:update');
-        $this->setNamespace('xmlns:domain', 'urn:ietf:params:xml:ns:domain-1.0', $create);
-        $this->setNamespace('xmlns:domain-ext', 'http://www.eurid.eu/xml/epp/domain-ext-2.5', $create);
+        $update = $this->createElement('domain-ext:update');
+        $update->setAttribute('xmlns:domain-ext', 'http://www.eurid.eu/xml/epp/domain-ext-2.5');
 
         if ($addnsgroup !== null) {
-            $this->processNsgroup($create, 'domain-ext:add', $addnsgroup);
+            $this->processNameservergroup($update, 'domain-ext:add', $addnsgroup);
         }
 
         if ($removensgroup !== null) {
-            $this->processNsgroup($create, 'domain-ext:rem', $removensgroup);
+            $this->processNameservergroup($update, 'domain-ext:rem', $removensgroup);
         }
 
-        $this->getExtension()->appendChild($create);
+        $this->getExtension()->appendChild($update);
     }
 
-    private function processNsgroup(DOMElement $parent, string $action, string|array $nsgroup): void
+    private function processNameservergroup(DOMElement $parent, string $action, string|array $nsgroup): void
     {
         if (is_array($nsgroup) && !empty($nsgroup)) {
             $element = $this->createElement($action);
