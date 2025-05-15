f6d53c55d359876b5310772947388a1d34db379a
Ewout de Graaf <ewout@metaregistrar.com>
2025-05-15 13:34:47 +0200

    metaregEppConnection changed order to load extensions

--- Registries/metaregEppConnection/eppConnection.php
+++ Registries/metaregEppConnection/eppConnection.php
@@ -9,13 +9,13 @@ class metaregEppConnection extends eppConnection {
 
         // Default server configuration stuff - this varies per connected registry
         // Check the greeting of the server to see which of these values you need to add
+        $this->enableDnssec();
+        $this->enableRgp();
         parent::useExtension('polldata-1.0');
         parent::useExtension('command-ext-1.0');
         parent::useExtension('ext-1.0');
         parent::useExtension('dns-ext-1.0');
         parent::useExtension('ssl-1.0');
-        $this->enableDnssec();
-        $this->enableRgp();
         //parent::enableLaunchphase('claims');
     }
 
