0533863c533d081b052bb4cac509dcdbc96752eb
Brianvanwessel <31928901+Brianvanwessel@users.noreply.github.com>
2025-05-28 10:17:55 +0200

    Update services of euridEppConnection

--- Registries/euridEppConnection/eppConnection.php
+++ Registries/euridEppConnection/eppConnection.php
@@ -7,7 +7,14 @@ class euridEppConnection extends eppConnection {
         parent::__construct($logging, $settingsfile);
 
         parent::enableDnssec();
-        parent::setServices(array('urn:ietf:params:xml:ns:domain-1.0' => 'domain', 'urn:ietf:params:xml:ns:contact-1.0' => 'contact','http://www.eurid.eu/xml/epp/registrarFinance-1.0'=>'registrar'));
+        parent::setServices(
+            [
+                'urn:ietf:params:xml:ns:domain-1.0' => 'domain',
+                'urn:ietf:params:xml:ns:contact-1.0' => 'contact',
+                'http://www.eurid.eu/xml/epp/registrarFinance-1.0' => 'registrar',
+                'http://www.eurid.eu/xml/epp/nsgroup-1.1' => 'nsgroup',
+            ]
+        );
         parent::useExtension('authInfo-1.1');
         parent::useExtension('domain-ext-2.5');
         parent::useExtension('contact-ext-1.3');
