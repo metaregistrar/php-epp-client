5c402b3eafb1e479a67609713041e903b646907d
Brian <b.vanwessel@exonet.nl>
2025-05-28 14:48:26 +0000

    Add comment to euridEppInfoNsgroupResponse

--- Protocols/EPP/eppExtensions/nsgroup-1.1/eppResponses/euridEppInfoNsgroupResponse.php
+++ Protocols/EPP/eppExtensions/nsgroup-1.1/eppResponses/euridEppInfoNsgroupResponse.php
@@ -2,6 +2,29 @@
 
 namespace Metaregistrar\EPP;
 
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+<epp
+    xmlns="urn:ietf:params:xml:ns:epp-1.0"
+    xmlns:nsgroup="http://www.eurid.eu/xml/epp/nsgroup-1.1">
+    <response>
+        <result code="1000">
+            <msg>Command completed successfully</msg>
+        </result>
+        <resData>
+            <nsgroup:infData>
+                <nsgroup:name>test1</nsgroup:name>
+                <nsgroup:ns>ns2.example.com</nsgroup:ns>
+                <nsgroup:ns>ns1.example.com</nsgroup:ns>
+            </nsgroup:infData>
+        </resData>
+        <trID>
+            <clTRID>6836ef66e5d2a</clTRID>
+            <svTRID>ef986e968-ebf7-479d-9822-ad8ebd47075d</svTRID>
+        </trID>
+    </response>
+</epp>
+*/
 /**
  * Class euridEppInfoNsgroupResponse
  * @package Metaregistrar\EPP
