47375bddfaddc3888513304fad6fd53fa85f6b97
Brian <b.vanwessel@exonet.nl>
2025-08-15 07:58:52 +0000

    Improve sidnEppInfoContactResponse

--- Protocols/EPP/eppExtensions/sidn-ext-epp-1.0/eppResponses/sidnEppInfoContactResponse.php
+++ Protocols/EPP/eppExtensions/sidn-ext-epp-1.0/eppResponses/sidnEppInfoContactResponse.php
@@ -6,8 +6,6 @@ class sidnEppInfoContactResponse extends eppInfoContactResponse
 {
     public function getLegalForm()
     {
-        $xpath = $this->xPath();
-        $result = $xpath->query('/epp:epp/epp:response/epp:extension/sidn-ext-epp:ext/sidn-ext-epp:infData/sidn-ext-epp:contact/sidn-ext-epp:legalForm');
-        return $result->item(0)->nodeValue;
+        return $this->queryPath('/epp:epp/epp:response/epp:extension/sidn-ext-epp:ext/sidn-ext-epp:infData/sidn-ext-epp:contact/sidn-ext-epp:legalForm');
     }
 }
