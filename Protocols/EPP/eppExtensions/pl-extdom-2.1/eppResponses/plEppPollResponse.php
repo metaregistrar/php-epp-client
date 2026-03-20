93dfa0a16b7265d1359bc718518ec7d533472ed4
bessone <riccardo@bess.one>
2026-03-20 11:36:05 +0100

    Fix XML path references in plEppPollResponse for accurate domain name retrieval

--- Protocols/EPP/eppExtensions/pl-extdom-2.1/eppResponses/plEppPollResponse.php
+++ Protocols/EPP/eppExtensions/pl-extdom-2.1/eppResponses/plEppPollResponse.php
@@ -15,57 +15,57 @@ class plEppPollResponse extends eppPollResponse
 
     // BROKEN_DELEGATION
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:dlgData/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:dlgData/extdom:name');
     }
 
     // EXPIRATION_POSSIBLE
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:expData/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:expData/extdom:name');
     }
 
     // DOMAIN_AUTHINFO
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollAuthInfo/extdom:domain/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollAuthInfo/extdom:domain/extdom:name');
     }
 
     // DOMAIN_BLOCKED
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollDomainBlocked/extdom:domain/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollDomainBlocked/extdom:domain/extdom:name');
     }
 
     // DOMAIN_UNBLOCKED
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollDomainUnblocked/extdom:domain/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollDomainUnblocked/extdom:domain/extdom:name');
     }
 
     // DOMAIN_JUDICIAL_REMOVED
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollDomainJudicialRemoved/extdom:domain/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollDomainJudicialRemoved/extdom:domain/extdom:name');
     }
 
     // DOMAIN_AUTO_RENEWED
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollDomainAutoRenewed/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollDomainAutoRenewed/extdom:name');
     }
 
     // DOMAIN_AUTO_RENEW_FAILED
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollDomainAutoRenewFailed/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollDomainAutoRenewFailed/extdom:name');
     }
 
     // PREPAID_DOMAIN_LOCK
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollDomainLocked/extdom:domain/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollDomainLocked/extdom:domain/extdom:name');
     }
 
     // PREPAID_DOMAIN_UNLOCK
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollDomainUnlocked/extdom:domain/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollDomainUnlocked/extdom:domain/extdom:name');
     }
 
     // DOMAIN_DELEGATION_ON_HOLD
     if ($domainName === null) {
-      $domainName =  $this->queryPath('/epp:epp/epp:response/resData/extdom:pollDomainDelegationOnHold/extdom:domain/extdom:name');
+      $domainName =  $this->queryPath('/epp:epp/epp:response/epp:resData/extdom:pollDomainDelegationOnHold/extdom:domain/extdom:name');
     }
 
     return $domainName;
