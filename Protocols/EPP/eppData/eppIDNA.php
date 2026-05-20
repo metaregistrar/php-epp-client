d4e05de68751282d6dbc0dd2bb1c68365b722622
Gene Surov <esurov@gmail.com>
2026-05-20 14:35:22 +0200

    PHP 8.5 deprecation fix
    Non-canonical cast (binary) is deprecated, use the (string) cast instead.

--- Protocols/EPP/eppData/eppIDNA.php
+++ Protocols/EPP/eppData/eppIDNA.php
@@ -929,7 +929,7 @@ class eppIDNA {
         if (self::$_mb_string_overload) {
             return mb_strlen($string, '8bit');
         }
-        return strlen((binary)$string);
+        return strlen((string)$string);
     }
 
     /**
