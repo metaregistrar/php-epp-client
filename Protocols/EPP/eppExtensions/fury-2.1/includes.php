520e407142834c74a6047c513c58c371a97078f2
Ewout de Graaf <ewout@metaregistrar.com>
2026-07-02 13:39:57 +0200

    fury 2.1 properties request added

--- Protocols/EPP/eppExtensions/fury-2.1/includes.php
+++ Protocols/EPP/eppExtensions/fury-2.1/includes.php
@@ -4,7 +4,7 @@ $this->addExtension('fury-2.1','urn:ietf:params:xml:ns:fury-2.1');
 
 include_once(dirname(__FILE__) . '/eppRequests/furyPropertiesRequest.php');
 include_once(dirname(__FILE__) . '/eppResponses/furyPropertiesResponse.php');
-$this->addCommandResponse('Metaregistrar\EPP\furyPropertiesRequest', 'Metaregistrar\EPP\eppFuryPropertiesResponse');
+$this->addCommandResponse('Metaregistrar\EPP\furyPropertiesRequest', 'Metaregistrar\EPP\furyPropertiesResponse');
 
 include_once(dirname(__FILE__) . '/eppRequests/furyCreateDomainRequest.php');
 $this->addCommandResponse('Metaregistrar\EPP\furyCreateDomainRequest', 'Metaregistrar\EPP\eppCreateDomainResponse');
