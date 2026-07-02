15c6a1c6086dfc8a2d8ffd094b3ad8eab537283b
Ewout de Graaf <ewout@metaregistrar.com>
2026-07-02 13:21:19 +0200

    furyRgp documentation added

--- Protocols/EPP/eppExtensions/fury-rgp-1.0/includes.php
+++ Protocols/EPP/eppExtensions/fury-rgp-1.0/includes.php
@@ -1,4 +1,5 @@
 <?php
+// Documentation: https://www.cira.ca/en/resources/documents/registry/cira-fury-rgp-1-epp-extension/
 $this->addExtension('fury-rgp-1.0','urn:ietf:params:xml:ns:fury-rgp-1.0');
 
 include_once(dirname(__FILE__) . '/eppResponses/furyRgpInfoDomainResponse.php');
