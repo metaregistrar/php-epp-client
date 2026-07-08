2d0c7180d2a21becc0e5a0d5ec149e3a5183634a
Ewout de Graaf <ewout@metaregistrar.com>
2026-07-08 10:35:57 +0200

    Update reseller request

--- Protocols/EPP/eppExtensions/org-1.0/includes.php
+++ Protocols/EPP/eppExtensions/org-1.0/includes.php
@@ -18,7 +18,7 @@ include_once(dirname(__FILE__) . '/eppResponses/orgEppCreateResponse.php');
 $this->addCommandResponse('Metaregistrar\EPP\orgEppCreateRequest', 'Metaregistrar\EPP\orgEppCreateResponse');
 
 include_once(dirname(__FILE__) . '/eppRequests/orgEppUpdateRequest.php');
-$this->addCommandResponse('Metaregistrar\EPP\orgEppUpdateRequest', 'Metaregistrar\EPP\orgEppResponse');
+$this->addCommandResponse('Metaregistrar\EPP\orgEppUpdateRequest', 'Metaregistrar\EPP\eppUpdateResponse');
 
 include_once(dirname(__FILE__) . '/eppRequests/orgEppDeleteRequest.php');
 $this->addCommandResponse('Metaregistrar\EPP\orgEppDeleteRequest', 'Metaregistrar\EPP\eppDeleteResponse');
