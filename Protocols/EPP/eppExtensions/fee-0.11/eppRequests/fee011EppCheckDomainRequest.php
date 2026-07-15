ff6f0455296b1c65cb7f45428bce0ac3351dbff1
Ewout de Graaf <ewout@metaregistrar.com>
2026-07-15 16:11:06 +0200

    corrected fee-0.11 request and response

--- Protocols/EPP/eppExtensions/fee-0.11/eppRequests/fee011EppCheckDomainRequest.php
+++ Protocols/EPP/eppExtensions/fee-0.11/eppRequests/fee011EppCheckDomainRequest.php
@@ -2,29 +2,43 @@
 namespace Metaregistrar\EPP;
 
 class fee011EppCheckDomainRequest extends eppCheckDomainRequest {
-	function __construct($checkrequest, $namespacesinroot) {
+
+	const COMMAND_CREATE = 'create';
+	const COMMAND_RENEW = 'renew';
+	const COMMAND_TRANSFER = 'transfer';
+
+	function __construct($checkrequest, $command = 'create', $namespacesinroot=true) {
 		parent::__construct($checkrequest, $namespacesinroot);
-		$this->addFee(null,null);
+		// $command may be 'create', 'renew', 'transfer'
+		// $period is in years and may be 1 - 10
+		// $class is determined by the registry and can contain certain tiers of pricing
+		$this->addFee($checkrequest,$command);
 		$this->addSessionId();
 	}
 
-	private function addFee(eppDomain $domain, $command, $phase=null, $period = null) {
+	private function addFee(eppDomain $domain, string $command, ?string $class=null, ?int $period = null, ?string $currency = null) {
+		if (!in_array($command,['create','renew','transfer'])) {
+			throw new eppException('Invalid command for fee-0.11 extension. Valid commands are create, renew, transfer');
+		}
+		if ($period)  {
+			if (($period == 0) || ($period>10)) {
+				throw new eppException('Invalid period for fee-0.11 extension. Valid periods are 0-10');
+			}
+		}
 		$extension = $this->getExtension();
 		$check = $this->createElement('fee:check');
 		$check->setAttribute('xmlns:fee','urn:ietf:params:xml:ns:fee-0.11');
-		$fee = $this->createElement('fee:object');
-		$fee->setAttribute('objURI','urn:ietf:params:xml:ns:domain-1.0');
-		$objid = $this->createElement('fee:objID',$domain->getDomainname());
-		$objid->setAttribute('element','name');
-		$fee->appendChild($objid);
-		$command = $this->createElement('fee:command',$command);
-		if ($phase) {
-			$command->setAttribute('phase',$phase);
-			$fee->appendChild($phase);
+		$check->appendChild($this->createElement('fee:command',$command));
+		if ($currency) {
+			$check->appendChild($this->createElement('fee:currency',$currency));
+		}
+		if ($class) {
+			$check->appendChild($this->createElement('fee:class',$class));
 		}
 		if ($period) {
 			$per = $this->createElement('fee:period',$period);
 			$per->setAttribute('unit','y');
+			$check->appendChild($per);
 		}
 		$extension->appendChild($check);
 		return;
--- Protocols/EPP/eppExtensions/fee-0.11/eppResponses/fee011EppCheckDomainResponse.php
+++ Protocols/EPP/eppExtensions/fee-0.11/eppResponses/fee011EppCheckDomainResponse.php
@@ -4,17 +4,37 @@ namespace Metaregistrar\EPP;
 class fee011EppCheckdomainResponse extends eppCheckDomainResponse {
 
 	public function getFees() {
+		$result = [];
 		$xpath = $this->xPath();
-		$result = $xpath->query('/epp:epp/epp:response/epp:extension/fee:chkData/fee:cd/*');
-		if ($result->length > 0) {
-			if ($result->item(0)->nodeValue == 'true') {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return null;
+		$result['available'] = $xpath->query('/epp:epp/epp:response/epp:extension/fee:chkData/*')->item(0)->getAttribute('avail');
+		$list = $xpath->query('/epp:epp/epp:response/epp:extension/fee:chkData/fee:cd/*');
+		foreach ($list as $node) {
+			/* @var \DOMElement $node */
+			switch ($node->tagName) {
+				case 'fee:command':
+					$result['command'] = $node->nodeValue;
+					break;
+				case 'fee:fee':
+					$result['price'] = $node->nodeValue;
+					break;
+				case 'fee:class':
+					$result['class'] = $node->nodeValue;
+					break;
+				case 'fee:period':
+					$result['period'] = $node->nodeValue;
+					$periodunit = $node->getAttribute('unit');
+					$result['periodunit'] = $periodunit;
+					break;
+				case 'fee:currency':
+					$result['currency'] = $node->nodeValue;
+					break;
+				case 'fee:object':
+					$result['domainname'] = $node->getElementsByTagName('name')->item(0)->nodeValue;
+					break;
+			};
 		}
+		return $result;
 	}
+
 
 }
